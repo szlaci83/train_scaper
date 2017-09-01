4c15403c1ab8b083fbcb03e88a234d403310dba5
laszlo <laszlo@elementalconcept.com>
2017-09-01 14:28:49 +0100

    clean up code

--- scraper.py
+++ scraper.py
@@ -1,18 +1,30 @@
 import bs4 as bs
-import urllib.request
-import re
-import csv
+import urllib.request, re, csv, os.path
+
+stn = {}
+stn['LUT'] = ['KGX', 'BDM', 'NMP', 'LVC', 'GLC']
+stn['KGX'] = ['SOU', 'GTW', 'POO', 'MAN', 'LUT']
+stn['LVC'] = ['LVC', 'MAN', 'KGX']
+stn['SOU'] = ['KGX', 'POO']
+stn['POO'] = ['KGX', 'SOU']
+
+# The train site we are scraping
+URL = 'http://ojp.nationalrail.co.uk/service/timesandfares/'
+# The end parameters for te URL : day/time/direction
+END = '/today/1130/dep'
 
 def getTds(fromStn, toStn):
     '''Get the td tags'''
+
     print('scraping:' + fromStn+ "/" + toStn )
-    sauce = urllib.request.urlopen('http://ojp.nationalrail.co.uk/service/timesandfares/' +fromStn+'/'+toStn+ '/today/1130/dep').read()
+    sauce = urllib.request.urlopen(URL + fromStn + '/' + toStn + END).read()
     soup = bs.BeautifulSoup(sauce, "lxml")
     tds = soup.find_all('td', {"class" : "dur"})
     return tds
 
 def getDuration(tds):
     '''Get the duration information from td tags'''
+
     mins = 0
     hrs = 0
     if not tds:
@@ -33,17 +45,21 @@ def getDuration(tds):
     mins = int(hrs) * 60 + int(mins)
     return mins
 
-#print(getDuration(getTds("ABW","AVY")))
 
-def getStationDict():
+def getStationDict(file_name):
     '''create a dictionary from the station_codes .csv file'''
-    with open('station_codes.csv', mode='r') as infile:
+
+    if not os.path.isfile(file_name):
+        return stn
+    with open(file_name, mode='r') as infile:
         reader = csv.reader(infile)
         mydict = {rows[0]:rows[1] for rows in reader}
     return mydict
 
+
 def getAllDurations(CRS_codes):
     '''Get the duration for the CRS codes'''
+
     durations = []
     for fromStn in CRS_codes.keys():
         for toStn in CRS_codes[fromStn]:
@@ -56,6 +72,7 @@ def getAllDurations(CRS_codes):
 
 def toCSV(filename, durations):
     '''Save the data into a csv file'''
+
     with open(filename, 'w', newline='') as out:
         csv_out = csv.writer(out)
         csv_out.writerow(['From', 'to', 'duration'])
@@ -63,16 +80,6 @@ def toCSV(filename, durations):
             print(row)
             csv_out.writerow(row)
 
-stations = getStationDict()
-# print(stations["AVY"])
-keys = stations.keys()
-#print(getAllDurations(keys))
-stn = {}
-stn['LUT'] = ['KGX', 'BDM', 'NMP', 'LVC', 'GLC']
-stn['KGX'] = ['SOU', 'GTW', 'POO', 'MAN', 'LUT']
-stn['LVC'] = ['LVC', 'MAN', 'KGX']
-stn['SOU'] = ['KGX', 'POO']
-stn['POO'] = ['KGX', 'SOU']
-
-durations = getAllDurations(stn)
-toCSV('durations.csv', durations)
+if __name__ == "__main__":
+    durations = getAllDurations(getStationDict('station_codes.csv'))
+    toCSV('durations.csv', durations)
